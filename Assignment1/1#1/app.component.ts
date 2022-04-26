97ec726a5df69487720e14249095d86a7f5eed0d
Ravitejaragula <103405090+Ravitejaragula@users.noreply.github.com>
2022-04-26 12:49:28 +0530

    Add files via upload

---
+++ Assignment1/1#1/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+	users = [{name:'gowtham',age:20},{name:'surendra',age:22},{name:'Naveen',age:30}];
+
+	
+ }
+
